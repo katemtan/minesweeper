46cc630eebf6bccacf8d00b0a4e18092f0b37a13
Katie Tanner <katietanner0@gmail.com>
2021-08-02 23:15:53 -0400

    update path

--- src/features/game/Tile/Tile.test.js
+++ src/features/game/Tile/Tile.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Tile } from './Tile';
 import { Provider } from 'react-redux';
-import { store } from '../../app/store';
+import { store } from '../../../app/store';
 
 it('renders with adjacent bombs', () => {
    const tile = render(
